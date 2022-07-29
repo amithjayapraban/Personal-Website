c2698c3125e49e376800a5b60fef43f7a21a72c2
Amith Jayapraban <amithjayapraban@Amiths-MacBook-Air.local>
2022-07-29 22:36:36 +0530

    fixed flex and grids for all  screens

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,34 +4,36 @@ export default function About() {
   return (
     
       <Transition>
-      <div className=' text-fontcolor h-[90vh] overflow-y-hidden  px-7  text-left text-sm md:text-sm grid  grid-flow-row grid-rows-[auto,auto,auto] '>
-        <div className='col-start-2  col-end-3 row-start-2 md:col-start-2 md:col-end-3  flex'>
-        <div className='w-[150px] mb-5 md:mb-10 h-[150px]  ml-[0px]   justify-self-start border-double border-[0px]  border-fontcolor box  '>
-        </div>
-          <img className='z-10 w-[150px] translate-x-[-135px] translate-y-[15px] mb-5 md:mb-10 h-[150px]' src="/amith.jpeg" />
-        </div>
-        
-
-
-        
-          <div className='row-start-2 self-center mt-[8rem]  ml-[-30px] col-start-2 col-end-3'  >
-          <p className=' col-start-2 md:text-xl text-link text-lg col-end-3 px-7 text-left  '>Front-end developer from Kerala, India.</p>
-          <p className=' col-start-2 mt-1 mb-1 col-end-3 px-7 text-left ' >Passionate about Web and Photography.</p>
+  <div className=' text-fontcolor h-[90vh] overflow-y-hidden  px-7  text-left text-sm md:text-sm grid  grid-flow-row grid-rows-[.5fr,1fr,.5fr] grid-cols-[.5fr,1fr,.5fr] '>
+    <div className="flex flex-col row-start-2 col-start-2 justify-center items-center" >
+         <div className='row-start-2 mr-8   h-[160px] md:self-start   flex '>
+          <div className='w-[150px] mb-5 md:mb-10 h-[150px]  ml-[0px]   border-double border-[0px]  border-fontcolor box  '></div>
+          
+          <img className='z-10 w-[150px] translate-x-[-135px] translate-y-[15px] md:translate-y-[15px]  mb-5 md:mb-10 h-[150px]' src="/amith.jpeg" />
+          </div>
+        <div />
+          
+        <div className='row-start-2 mt-5 md:self-start  flex flex-col '  >
+          <p className='  md:text-xl  text-link text-sm   '>Front-end developer from Kerala, India.</p>
+          <p className=' mt-1 mb-1  text-xs md:text:sm ' >Passionate about Web and Photography.</p>
             {/* <p className=' col-start-2 col-end-3 px-7 text-left '>Currently pursuing MCA from College of Engineering, Trivandrum.</p>
            */}
-          </div>
-          <div className="self-end row-start-2  col-start-2 ml-[0px] cursor-pointer col-end-3  flex justify-start">
-       <a href="https://www.linkedin.com/in/amithjayapraban/" target='_blank' className='link cursor-pointer rounded-[50%] linkedin'>
-       <img src="/linkedin.svg" />
-       </a>
-       <a href="https://github.com/amithjayapraban" target='_blank'  className='link ml-4 mr-4 github'>
-       <img src="/github.svg" />
-       </a> 
-       <a href="https://instagram.com/amithjayapraban" target='_blank'  className='link insta'>
-       <img src="/instagram.svg" />
-       </a> 
-       </div> 
         </div>
+          
+
+      <div className="row-start-2 self-start mt-3 col-start-2 ml-[0px] cursor-pointer col-end-3  flex justify-start">
+       <a href="https://www.linkedin.com/in/amithjayapraban/" target='_blank' className='link cursor-pointer rounded-[50%] linkedin'>
+       <img src="/linkedin.svg" /></a>
+       
+       <a href="https://github.com/amithjayapraban" target='_blank'  className='link ml-4 mr-4 github'>
+       <img src="/github.svg" /></a> 
+       
+       <a href="https://instagram.com/amithjayapraban" target='_blank'  className='link insta'>
+       <img src="/instagram.svg" /></a> 
+       
+      </div> 
+    </div>
+  </div>
       </Transition>
     
     
