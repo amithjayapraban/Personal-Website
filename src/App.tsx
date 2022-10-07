3b2c8cd4038b9ac3ac58b35056cef15f663d0b17
Amith Jayapraban <amithjayapraban@Amiths-MacBook-Air.local>
2022-10-07 23:59:33 +0530

    added helmet-async

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,17 @@ import Head from "./pages/Head";
 import Foot from "./pages/Foot";
 import ScrollToTop from "./ScrollToTop";
 import Scene from "./Scene";
+
 function App() {
   const [theme, setTheme]: any = useState("light");
 
   useEffect(() => {
     var t: any = localStorage.getItem("data-theme");
-    
-    if(t){document.documentElement.setAttribute("data-theme", t); setTheme(t);}
-    else{
+
+    if (t) {
+      document.documentElement.setAttribute("data-theme", t);
+      setTheme(t);
+    } else {
       document.documentElement.setAttribute("data-theme", theme);
     }
   }, []);
@@ -39,38 +42,50 @@ function App() {
       localStorage.setItem("data-theme", "light");
     }
   }
-
+  var type = "article";
+  var name = "Amith Jayapraban";
+  var title="Amith Jayapraban";
+  var description="Frontend developer from Kerala, India 🇮🇳";
   return (
-    //bg-primary
-    <div className="App bg-transparent  overflow-y-hidden
-     text-fontcolor  ">
-     
+    <div
+      className="App bg-transparent  overflow-y-hidden
+     text-fontcolor  "
+    >
       <Helmet>
-        <meta charSet="utf-8" />
-        <title>Amith Jayapraban</title>
-        <link
-          rel="icon"
-          href="https://avatars.githubusercontent.com/u/89343885?v=4"
-        />
-        <meta
-          name="description"
-          content="Frontend developer from Kerala, India 🇮🇳"
-        />
-        <meta name="theme-color" content="#34c592" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="robots" content="index,follow" />
-        <meta name="googlebot" content="index,follow" />
-        <link rel="canonical" href="http://amithjayapraban.tk" />
-      </Helmet>
+      <title>{title} </title>
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+
+      <link
+        rel="icon"
+        href="https://avatars.githubusercontent.com/u/89343885?v=4"
+      />
+
+      <meta name="theme-color" content="#34c592" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="robots" content="index,follow" />
+      <meta name="googlebot" content="index,follow" />
+      <link rel="canonical" href="http://amithjayapraban.tk" />
+
+     
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+    
+      <meta name="twitter:creator" content={name} />
+      <meta name="twitter:card" content={type} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+    </Helmet>
+
       <Router>
-        <Head Changetheme={Changetheme} theme={theme}/>
+        <Head Changetheme={Changetheme} theme={theme} />
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
-    
     </div>
   );
 }
