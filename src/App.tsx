b264a96917d1656bd1476cb7fffa1ef028f99f0b
Amith Jayapraban <amithjayapraban@Amiths-MacBook-Air.local>
2023-01-11 20:53:06 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -50,10 +50,7 @@ function App() {
           <title>{title} </title>
           <meta name="description" content={description} />
           <meta charSet="utf-8" />
-
           <link rel="icon" href="../amith.webp" />
-
-          <meta name="theme-color" content="#34c592" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
