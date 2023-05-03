975fe736b09660953f213787a2e579e67c6c77b8
Amith Jayapraban <amithjayapraban@Amiths-MacBook-Air.local>
2023-05-03 22:09:38 +0530

    fixes for empty posts folder

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -3,23 +3,27 @@ import path from "path";
 import matter from "gray-matter";
 
 const postsDirectory = path.join(process.cwd(), "posts");
-
+console.log(fs.readdirSync(postsDirectory),'zxzx');
 export function getSortedPostsData() {
   const fileNames = fs.readdirSync(postsDirectory);
+  if(fileNames.length>1){
   const allPostsData = fileNames.map((fileName) => {
+   
     const slug = fileName.replace(/\.md$/, "");
+
 
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, "utf8");
 
     const matterResult = matter(fileContents);
-   
+    
     return {
     slug,
       ...matterResult.data,
     };
+  
   });
-
+  
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
       return 1;
@@ -27,4 +31,8 @@ export function getSortedPostsData() {
       return -1;
     }
   });
+}
+else{
+  return null
+}
 }
--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -19,7 +19,7 @@ export default function Directory({ allPostsData }: any) {
     <section className="gap-2 flex flex-col py-16 p-4 text-fontcolor">
       <h1 className="text-md font-bold md:text-xl text-text_h">Posts</h1>
       <ul className="flex flex-col  h-full gap-2">
-        {allPostsData.length > 0 &&
+        {allPostsData &&allPostsData.length > 0 &&
           allPostsData.map(({ slug, date, title }: any) => (
             <Card key={slug} p={"p-4 py-6"} w={"w-full"}>
               <li className="flex text-xs items-center justify-between">
@@ -31,7 +31,7 @@ export default function Directory({ allPostsData }: any) {
             </Card>
           ))}
       </ul>
-      {allPostsData.length == 0 && (
+      {!allPostsData && (
         <section className="gap-2 flex flex-col items-center py-16 p-4 text-fontcolor">
           <h1 className="">😕</h1>
         </section>
